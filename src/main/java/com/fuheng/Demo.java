caab39b66b0fc6db0396356ff150c945ebf0a0af
FuHeng <2455788801@qq.com>
2023-02-16 13:44:03 +0800

    first commit

---
+++ src/main/java/com/fuheng/Demo.java
@@ -0,0 +1,12 @@
+package com.fuheng;
+
+/**
+ * @Author: fuheng
+ * @Description:
+ * @DateTime: 2023/2/16 13:43
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
