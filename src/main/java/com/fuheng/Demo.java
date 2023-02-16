f3481a85475fc24ef8fae6ace7b4f8947ada9917
FuHeng <74184656+FuHengYa@users.noreply.github.com>
2023-02-16 14:44:27 +0800

    pulltest

--- src/main/java/com/fuheng/Demo.java
+++ src/main/java/com/fuheng/Demo.java
@@ -7,7 +7,7 @@ package com.fuheng;
  **/
 public class Demo {
     public static void main(String[] args) {
-        System.out.println(1);
+        
         System.out.println(2);
         System.out.println("hot-fix");
         System.out.println("hot-fix test");
